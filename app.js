0a66e1ae49328b14de4cb39b9805d5b4e33c370e
Shawn10804 <shawn10804@gmail.com>
2023-08-17 09:21:43 +0200

    few changes made

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-// MongoDB Atlas connection settings
 mongoose.connect('mongodb+srv://shawn1080444:x8FVoW53VDBCO7H4@cluster1.np9fyen.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
-
 const mongoURI = 'mongodb+srv://shawn1080444:x8FVoW53VDBCO7H4@cluster1.np9fyen.mongodb.net/';
 const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // create variable that will hold the connection and tell you whether you connected or not.
 const db = mongoose.connection;
-// This is for login
+
 const validUser = {
   username: 'user123',
   email: 'mzwa@gmail.com',
@@ -24,13 +22,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public')); // Serve static assets like CSS files
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// This use
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-// Define a subscriber schema
 const subscriberSchema = new mongoose.Schema({
   name : { type: String},
   number : { type: String},
@@ -72,7 +68,6 @@ app.post('/beneficiaries', (req, res) => {
   const message = req.body.motivation;
   // Create a new Subscriber document
   const beneficiaries = new Beneficiary({ name,surname,address, id_number, numbers, grade, message});
-  // Save the subscriber to the database
   beneficiaries.save()
   .then(() => {
     res.send('Thank you ' + name + ' for applying as our beneficiary, we\'ll get back to you shortly!');
@@ -87,12 +82,9 @@ app.get('/contact', (req,res)=> {
   res.render('contact');
 })
 
-
 app.get('/login', (req,res)=> {
   res.render('log');
 })
-
-
 
 app.post('/dashboard', async(req, res) => {
   const { username, password, email } = req.body;
@@ -113,8 +105,6 @@ app.post('/dashboard', async(req, res) => {
   } finally {
     await client.close();
   }
-
-  // Check if entered credentials match the valid user
   } else {
     res.status(401).send('Invalid credentials. Please try again.');
   }
@@ -136,7 +126,6 @@ app.get('/contact-us', async (req, res)=> {
     await client.close();
   }
 
-  // Check if entered credentials match the valid user
   })
   
 app.get('/subscribers', async (req,res)=> {
@@ -163,7 +152,6 @@ app.post('/Contact', (req, res) => {
   const message = req.body.message;
   // Create a new Subscriber document
   const contact = new ContactUs({ name, email , number, message });
-  // Save the subscriber to the database
   contact.save()
   .then(() => {
     res.send('Thank you ' + name + ' for contacting us, we\'ll get back to you shortly!');
@@ -175,17 +163,11 @@ app.post('/Contact', (req, res) => {
 
 });
 
-
-
-  // Handle form submission
   app.post('/subscribe', (req, res) => {
     const email = req.body.email;
-    // Create a new Subscriber document
     const subscriber = new Subscriber({ email });
-    // Save the subscriber to the database
     subscriber.save()
     .then(() => {
-  
       console.log('Thank you for subscribing, subscriber added as : ' + email);
       res.render('subscribed');
     })
